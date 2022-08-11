d8b7bf42ed05f5bcb68dfe612d7f59e6ae71f2d4
Kuba <kjuchnowski@gmail.com>
2022-08-11 22:29:28 +0200

    Added new features, working to put everything together working.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import random
 import pandas as pd
 
@@ -17,19 +18,72 @@ deck = [
     {"card": "Ace", "count": 4, "points": (1, 11)}
 ]
 
-deck_df = pd.DataFrame(deck)
-print(deck_df['card'])
+# deck_df = pd.DataFrame(deck)
 
 
-def update_deck(player_set):
-    global deck_df
-    if player_set['card'] == deck_df['card']:
-        deck_df['count'] = deck_df['count'] - player_set['count']
+# def update_deck(player_set):
+#     global deck_df
+#     card_names = player_set.card
+#     index_ = np.where(deck_df.card == card_names)
+        
     
     
-def distribute_cards(amount):
+def distribute_cards(amount: int) -> list:
     player_set = random.choices(deck, k=amount)
     for items in player_set:
         items['count'] = 1
-    return pd.DataFrame(player_set)
+    return player_set
 
+
+def its_ace():
+    chosen_point = int(input('Do you want to choose 1 or 11?\n>>'))
+    if chosen_point == 1:
+        return 1
+    elif chosen_point == 11:
+        return 11
+    else:
+        print("Invalid point.")
+
+
+def count_score(player_set: list) -> int:
+    score = 0
+    for items in player_set:
+        if items['card'] != 'Ace':
+            score += items['points']
+        else:
+            score += its_ace()
+    return score
+
+
+def game():
+    p1_set = distribute_cards(amount=2)
+    p2_set = distribute_cards(amount=2)
+    p1_score = count_score(p1_set)
+    p2_score = count_score(p2_set)
+    who_first = random.choice(['p1', 'p2'])
+    if who_first == 'p1':
+        print("PLAYER 1 STARTS\n")
+        while p1_score <= 21:
+            keep_going = input("Do you want to take another card?\n>>").lower()
+            if 'y' in keep_going:
+                p1_set.append(distribute_cards(1))
+                p1_score = count_score(p1_set)
+                print(f"\nSCORE: {p1_score}")
+            else:
+                break
+            if p1_score > 21:
+                print(f"\nTHE GAME IS OVER\tPLAYER 1 WINS!")
+    else:
+        print("PLAYER 2 STARTS\n")
+        while p2_score <= 21:
+            keep_going = input("Do you want to take another card?\n>>").lower()
+            if 'y' in keep_going:
+                p2_set.append(distribute_cards(1))
+                p2_score = count_score(p2_set)
+                print(f"\nSCORE: {p2_score}")
+            if p2_score > 21:
+                print(f"\nTHE GAME IS OVER\tPLAYER 1 WINS!")
+        
+
+
+game()
