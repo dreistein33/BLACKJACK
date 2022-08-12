ef5592d93cac8e4cb7f8d329dae338fd05594a18
Kuba <kjuchnowski@gmail.com>
2022-08-12 18:19:02 +0200

    Finished writing the logic, the game is now playable.

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
 import deck
 import player
-
+import random
+import sys
 
 class Game:
     def __init__(self):
         self.deck = deck.Deck()
-        self.player1 = player.Player(self.deck, "Wojtek")
+        self.player1 = player.Player(self.deck, "Dealer")
         self.player2 = player.Player(self.deck, "Adam")
 
+    def check_who_first(self):
+        players = [self.player1, self.player2]
+        return random.choice(players)
+
+    def player_turn(self, player):
+        while True:
+            choice = input(f"SCORE: {player.score} # {player.name} # ADD CARD? [yes/no]\n>>").lower()
+            if choice == 'yes':
+                player.add_card()
+                print(f"{player.name} -> {player.score}")
+            elif choice == 'no':
+                print(f"{player.name} -> {player.score}")
+                break
+            if player.score > 21:
+                print(f'{player.name} LOSE!')
+                sys.exit(0)
+    
+    def compare_score(self):
+        if self.player1.score > self.player2.score:
+            print(f"\n\t\t\t\t{self.player1.name} WINS!")
+        else:
+            print(f"\n\t\t\t\t{self.player2.name} WINS!")
+
+    def display_start(self, player):
+        greeting = "\t\t\t\tTHE GAME OF BLACKJACK!\n"
+        start = f"\t\t\t\t{player.name} STARTS -> {player.score}\n"
+        print(greeting)
+        print(start)
+
     def start(self):
-        print("\t\t\t\tTHE GAME OF BLACKJACK")
-        print(f"\n\t\t\t\t{self.player1.name} STARTS!")
+        if self.check_who_first() is self.player1:
+            self.display_start(self.player1)
+            self.player_turn(self.player1)
+            self.player_turn(self.player2)
+        else:
+            self.display_start(self.player2)
+            self.player_turn(self.player2)
+            self.player_turn(self.player1)
+        self.compare_score()
         
 
 if __name__ == '__main__':
--- player.py
+++ player.py
@@ -7,23 +7,21 @@ class Player:
     def __init__(self, deck, name):
         self.deck = deck
         self.name = name
-        self.score = 0
         self.set = self.deck.make_set(2)
+        self.score = self.return_points()
 
     def add_card(self):
         self.set.append(self.deck.make_set(1)[0])
+        self.score = self.return_points()
 
-    def count_score(self):
-        for i in self.set:
-            self.score += i['points']
-            check_thread = threading.Thread(target=self.check_score)
-            check_thread.start()
-        print(f"{self.name} -> {self.score}")
-
-    def check_score(self):
-        if self.score > 21:
-            print(f"{self.name} LOSE")
-            sys.exit(0)
+    def return_points(self):
+        points = [x['points'] for x in self.set]
+        score = sum(points)
+        return score
+    # def check_score(self):
+    #     if self.score > 21:
+    #         print(f"{self.name} LOSE")
+            
 
 
 if __name__ == '__main__':
@@ -32,4 +30,4 @@ if __name__ == '__main__':
     print(p1.set)
     p1.add_card()
     print(p1.set)
-    p1.count_score()
+    
